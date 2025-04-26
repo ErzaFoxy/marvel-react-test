661d81c2e6ec2db833531d8b7c2fd09fca0bcc6e
Elizabeth <erzafoxy55565@gmail.com>
2025-04-27 00:23:59 +0300

    Updates

---
+++ precache-manifest.a838704206c4a14422f13c736c681172.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49e26cd5963cc94d9f83b674f1ed302",
+    "url": "/marvel-react-test/index.html"
+  },
+  {
+    "revision": "82e288696ed24fc5c2d8",
+    "url": "/marvel-react-test/static/css/main.a791d2dd.chunk.css"
+  },
+  {
+    "revision": "db51ac83cac2498b615c",
+    "url": "/marvel-react-test/static/js/2.ccebcb33.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/marvel-react-test/static/js/2.ccebcb33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82e288696ed24fc5c2d8",
+    "url": "/marvel-react-test/static/js/main.aac68937.chunk.js"
+  },
+  {
+    "revision": "8cde9003aed490d9d57f",
+    "url": "/marvel-react-test/static/js/runtime-main.6f1db50d.js"
+  },
+  {
+    "revision": "6617f447369d9c9491b67bb82766110e",
+    "url": "/marvel-react-test/static/media/error.6617f447.gif"
+  },
+  {
+    "revision": "dd8446a3584cb7275fb1f2f31ab4c950",
+    "url": "/marvel-react-test/static/media/mjolnir.dd8446a3.png"
+  },
+  {
+    "revision": "76e8b3e15712a0d9c704b99e10476e4c",
+    "url": "/marvel-react-test/static/media/vision.76e8b3e1.png"
+  }
+]);
